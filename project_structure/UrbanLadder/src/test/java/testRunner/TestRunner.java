f5941d56b5d6b30cd6ca407359e2c9668a8ac926
Ramprasath <Ramprasath.s@cognizant.com>
2026-05-20 13:59:59 +0530

    testing

--- project_structure/UrbanLadder/src/test/java/testRunner/TestRunner.java
+++ project_structure/UrbanLadder/src/test/java/testRunner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        features = ".//Features",
+        features = ".//Features/Collections.feature",
         glue ="stepDefinitions",
         plugin = {"pretty", "html:reports/myreport.html"}
 )
